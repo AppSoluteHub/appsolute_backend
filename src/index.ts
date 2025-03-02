95c923531ee8e552ec3e45b1f797a8c759077296
solowiseCv <uchesolomon61@gmail.com>
2025-03-02 12:02:46 +0100

    port updated

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,3 @@
-// import express, { Request, Response } from "express";
-// import { Router } from "express";
-// import bodyParser from "body-parser";
-// import cors from "cors";
-// import cookieParser from "cookie-parser";
-// import multer from "multer";
-// import baseRoutes from "./features/appRoute";
-// import setupSwagger from "./swagger/swagger";
-// import session from "express-session";
-// import passport from "passport";
-// import googleRoute from "./middlewares/auth.google.middleware";
-// import dotenv from "dotenv";
-// import { errorHandler } from "./middlewares/error.middleware";
-// dotenv.config();
-
-
-// const app = express();
-// const port = process.env.PORT;
-
-
-// app.use(bodyParser.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:3001",
-     
-//     ], 
-//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials: true, 
-//   })
-// );
-
-// app.options("*", cors());
-
-// app.use(cookieParser());
-// app.use(express.json());
-
-// const storage = multer.memoryStorage();
-// export const upload = multer({ storage });
-
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET as string,
-//     resave: false,
-//     saveUninitialized: true,
-//     //     cookie: { secure: false },
-//   })
-// );
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-// app.use("/", googleRoute);
-
-// const router = Router();
-// const rootRouter = baseRoutes(router);
-// app.use("/api/v1", rootRouter);
-// app.use(errorHandler)
-
-
-// setupSwagger(app);
-
-// app.listen(port, () => console.log(`🚀 Server is firing on port ${port}`));
-
-// export default app;
-
 
 import express from "express";
 import { Router } from "express";
@@ -87,7 +19,7 @@ import http from "http";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app); 
 
 
