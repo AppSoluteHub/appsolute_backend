a9b6fe853d7659d8b0b17ea657291559800b0812
solowiseCv <uchesolomon61@gmail.com>
2025-03-12 00:49:21 +0100

    fix: comment out authentication middleware in user task route

--- src/features/tasks/routes/userTask.route.ts
+++ src/features/tasks/routes/userTask.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { answerTaskHandler } from "../controllers/userTask.controller";
-import authenticate from "../../../middlewares/auth.middleware";
+// import authenticate from "../../../middlewares/auth.middleware";
 
 const router = Router();
 
