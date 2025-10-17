6e7c8b260cf69d8b1314ea0e8ef66b84047674fc
solowiseCv <uchesolomon61@gmail.com>
2025-10-17 12:34:38 +0100

    401 in payment

--- src/features/software/routes/software.route.ts
+++ src/features/software/routes/software.route.ts
@@ -3,7 +3,6 @@ import * as softwareController from '../controllers/software.controller';
 import { validateRequest } from '../../../middlewares/validateRequest';
 import { createSoftwareDto, updateSoftwareDto } from '../dto/software.dto';
 import authenticate, { isAdmin } from '../../../middlewares/auth.middleware';
-import { is } from 'cheerio/dist/commonjs/api/traversing';
 
 const router = Router();
 
